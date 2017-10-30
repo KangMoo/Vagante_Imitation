645b4284ff2c0c6f0e0de641855d67984bfd228d
JuseopKim <kjss9324@naver.com>
2017-10-30 18:58:23 +0900

    맨이터 끝, 고블린 시작 이미지 위치 변경, 이미지 이름 변경

---
+++ TeamProj_Vagante/manEater.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Enemy.h"
+class manEater :
+	public Enemy
+{
+private:
+	Enemy _enemy;
+
+	RECT _attackRange;
+
+	int  _count;
+
+	bool _attack;
+
+public:
+	HRESULT init(POINT point);
+	void release();
+	void update();
+	void render();
+	void render(POINT camera);
+	void draw(POINT camera);
+
+	void attack();			// АјАн
+
+	RECT getAttackRange() { return _attackRange; }
+
+	manEater();
+	~manEater();
+};
+
