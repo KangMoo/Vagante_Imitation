e8c06653735829b6e37b36bfeb4508ced2220e91
jmh <wraith12@naver.com>
2017-11-01 19:03:35 +0900

    더러운 지렁이!

--- TeamProj_Vagante/worm.cpp
+++ TeamProj_Vagante/worm.cpp
@@ -10,6 +10,27 @@ worm::worm()
 worm::~worm()
 {
 }
+/*
+struct tagStat {
+	int hp;		//체력
+	int str;	//힘
+	int dex;	//민첩
+	int vit;	//활력
+	int inl;	//지능
+	int lck;	//운
+	int def;	//방어력
+	int fir;	//불저항
+	int ice;	//얼음저항
+	int lgt;	//빛저항
+	int psn;	//독저항
+	int mel;	//근접공격데미지
+	int rng;	//활공격데미지
+	int crit;	//크리티컬확률
+	int aspd;	//공속
+	int spd;	//이속
+};
+*/
+
 HRESULT worm::init(POINT point, float minCog, float maxCog)
 {
 	_image = IMAGEMANAGER->findImage("wormMoveUp");
@@ -20,7 +41,7 @@ HRESULT worm::init(POINT point, float minCog, float maxCog)
 	_pointx = point.x;
 	_pointy = point.y;
 	
-	_frameFPS = 0;
+	_frameFPS = 10.0f;
 	_frameTime = 0;
 	_currentFrameX = _currentFrameY = 0;
 	
@@ -52,7 +73,24 @@ HRESULT worm::init(POINT point, float minCog, float maxCog)
 	
 	_rc = RectMakeCenter(_pointx, _pointy, _image->getFrameWidth(), _image->getFrameHeight());
 	_attackRect = RectMakeCenter(_pointx, _pointy, 1, 1);
-	
+
+	_statistics.hp = 10;
+	_statistics.str = 2;
+	_statistics.dex = 2;
+	_statistics.vit = 2;
+	_statistics.inl = 2;
+	_statistics.lck = 2;
+	_statistics.def = 2;
+	_statistics.fir = 0;
+	_statistics.ice = 0;
+	_statistics.lgt = 0;
+	_statistics.psn = 0;
+	_statistics.mel = 0;
+	_statistics.rng = 0;
+	_statistics.crit = 0;
+	_statistics.aspd = 0;
+	_statistics.spd = 1;
+
 	//0 = 바닥, 1 = 왼쪽벽, 2 = 위쪽벽, 3 = 오른쪽벽
 	_whereIsWorm = 0;
 	
@@ -70,20 +108,22 @@ void worm::move()
 		case 0:
 			if (_isLeft)
 			{
-				if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx - 1) / TILESIZE).type != 1 &&
-					_map->getMapInfo((_pointy) / TILESIZE + 1, (_pointx - 1) / TILESIZE).type != 1)
+				if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx - _moveSpeed) / TILESIZE).type != 1 &&
+					_map->getMapInfo((_pointy) / TILESIZE + 1, (_pointx - _moveSpeed) / TILESIZE).type != 1)
 				{
 					//벽이 바로 앞과 앞에 아래에도 없을때
-					_whereIsWorm = 3;
+					_whereIsWorm = 1;
 					_image = IMAGEMANAGER->findImage("wormMoveLeft");
 					_pointx = _map->getMapInfo((_pointy) / TILESIZE + 1, (_pointx) / TILESIZE).rc.left - _image->getFrameWidth() / 2;
+					_pointy = _map->getMapInfo((_pointy) / TILESIZE + 1, (_pointx) / TILESIZE).rc.top;
 				}
-				else if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx - 1) / TILESIZE).type == 1)
+				else if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx - _moveSpeed) / TILESIZE).type == 1)
 				{
 					//바닥에서 왼쪽으로 갈 때, 벽이 바로 앞에 있는 경우
-					_whereIsWorm = 1;
+					_whereIsWorm = 3;
 					_image = IMAGEMANAGER->findImage("wormMoveRight");
-					_pointx = _map->getMapInfo((_pointy) / TILESIZE, (_pointx - 1) / TILESIZE).rc.right + _image->getFrameWidth() / 2;
+					_pointx = _map->getMapInfo((_pointy) / TILESIZE, (_pointx - _moveSpeed) / TILESIZE).rc.right + _image->getFrameWidth() / 2;
+					_pointy = _map->getMapInfo((_pointy) / TILESIZE, (_pointx - _moveSpeed) / TILESIZE).rc.bottom;
 				}
 				else
 				{
@@ -92,20 +132,22 @@ void worm::move()
 			}
 			else
 			{
-				if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx + 1) / TILESIZE).type != 1 &&
-					_map->getMapInfo((_pointy) / TILESIZE + 1, (_pointx + 1) / TILESIZE).type != 1)
+				if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx + _moveSpeed) / TILESIZE).type != 1 &&
+					_map->getMapInfo((_pointy) / TILESIZE + 1, (_pointx + _moveSpeed) / TILESIZE).type != 1)
 				{
 					//벽이 바로 앞과 앞의 아래에도 없을때
-					_whereIsWorm = 1;
+					_whereIsWorm = 3;
 					_image = IMAGEMANAGER->findImage("wormMoveRight");
 					_pointx = _map->getMapInfo((_pointy) / TILESIZE + 1, (_pointx) / TILESIZE).rc.right - _image->getFrameWidth() / 2;
+					_pointy = _map->getMapInfo((_pointy) / TILESIZE + 1, (_pointx) / TILESIZE).rc.top;
 				}
-				else if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx + 1) / TILESIZE).type == 1)
+				else if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx + _moveSpeed) / TILESIZE).type == 1)
 				{
 					//벽이 바로 앞에 없는 경우
-					_whereIsWorm = 3;
+					_whereIsWorm = 1;
 					_image = IMAGEMANAGER->findImage("wormMoveLeft");
-					_pointx = _map->getMapInfo((_pointy) / TILESIZE, (_pointx + 1) / TILESIZE).rc.left - _image->getFrameWidth() / 2;
+					_pointx = _map->getMapInfo((_pointy) / TILESIZE, (_pointx + _moveSpeed) / TILESIZE).rc.left - _image->getFrameWidth() / 2;
+					_pointy = _map->getMapInfo((_pointy) / TILESIZE, (_pointx + _moveSpeed) / TILESIZE).rc.bottom;
 				}
 				else
 				{
@@ -116,20 +158,22 @@ void worm::move()
 		case 1:
 			if (_isLeft)
 			{
-				if (_map->getMapInfo((_pointy - 1) / TILESIZE, (_pointx) / TILESIZE).type != 1 &&
-					_map->getMapInfo((_pointy - 1) / TILESIZE, (_pointx) / TILESIZE + 1).type != 1)
+				if (_map->getMapInfo((_pointy - _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).type != 1 &&
+					_map->getMapInfo((_pointy - _moveSpeed) / TILESIZE, (_pointx) / TILESIZE + 1).type != 1)
 				{
 					//바로 앞쪽 위에 벽이 없고 그 아래도 없는 경우
-					_whereIsWorm = 0;
+					_whereIsWorm = 2;
 					_image = IMAGEMANAGER->findImage("wormMoveUp");
 					_pointy = _map->getMapInfo((_pointy) / TILESIZE, (_pointx) / TILESIZE - 1).rc.top - _image->getFrameHeight() / 2;
+					_pointx = _map->getMapInfo((_pointy) / TILESIZE, (_pointx) / TILESIZE - 1).rc.right;
 				}
-				else if (_map->getMapInfo((_pointy - 1) / TILESIZE, (_pointx) / TILESIZE).type == 1)
+				else if (_map->getMapInfo((_pointy - _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).type == 1)
 				{
 					//바로 앞에 벽이 있을때
-					_whereIsWorm = 2;
+					_whereIsWorm = 0;
 					_image = IMAGEMANAGER->findImage("wormMoveDown");
-					_pointy = _map->getMapInfo((_pointy - 1) / TILESIZE, (_pointx) / TILESIZE).rc.bottom + _image->getFrameHeight() / 2;
+					_pointy = _map->getMapInfo((_pointy - _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).rc.bottom + _image->getFrameHeight() / 2;
+					_pointx = _map->getMapInfo((_pointy - _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).rc.left;
 				}
 				else
 				{
@@ -138,20 +182,22 @@ void worm::move()
 			}
 			else
 			{
-				if (_map->getMapInfo((_pointy + 1) / TILESIZE, (_pointx) / TILESIZE).type != 1 &&
-					_map->getMapInfo((_pointy + 1) / TILESIZE, (_pointx) / TILESIZE - 1).type != 1)
+				if (_map->getMapInfo((_pointy + _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).type != 1 &&
+					_map->getMapInfo((_pointy + _moveSpeed) / TILESIZE, (_pointx) / TILESIZE - 1).type != 1)
 				{
 					//앞에 벽 없고 아래도 벽 없음
-					_whereIsWorm = 2;
+					_whereIsWorm = 0;
 					_image = IMAGEMANAGER->findImage("wormMoveDown");
 					_pointy = _map->getMapInfo((_pointy) / TILESIZE, (_pointx) / TILESIZE - 1).rc.bottom + _image->getFrameHeight() / 2;
+					_pointx = _map->getMapInfo((_pointy) / TILESIZE, (_pointx) / TILESIZE - 1).rc.left;
 				}
-				else if (_map->getMapInfo((_pointy + 1) / TILESIZE, (_pointx) / TILESIZE).type == 1)
+				else if (_map->getMapInfo((_pointy + _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).type == 1)
 				{
 					//앞에 벽 있음
-					_whereIsWorm = 0;
+					_whereIsWorm = 2;
 					_image = IMAGEMANAGER->findImage("wormMoveUp");
-					_pointy = _map->getMapInfo((_pointy + 1) / TILESIZE, (_pointx) / TILESIZE).rc.top - _image->getFrameHeight() / 2;
+					_pointy = _map->getMapInfo((_pointy + _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).rc.top - _image->getFrameHeight() / 2;
+					_pointx = _map->getMapInfo((_pointy + _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).rc.right;
 				}
 				else
 				{
@@ -162,18 +208,20 @@ void worm::move()
 		case 2:
 			if (_isLeft)
 			{
-				if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx + 1) / TILESIZE).type != 1 &&
-					_map->getMapInfo((_pointy) / TILESIZE - 1, (_pointx + 1) / TILESIZE).type != 1)
-				{
-					_whereIsWorm = 3;
-					_image = IMAGEMANAGER->findImage("wormMoveLeft");
-					_pointx = _map->getMapInfo((_pointy) / TILESIZE - 1, (_pointx) / TILESIZE).rc.right + _image->getFrameWidth() / 2;
-				}
-				else if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx + 1) / TILESIZE).type == 1)
+				if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx + _moveSpeed) / TILESIZE).type != 1 &&
+					_map->getMapInfo((_pointy) / TILESIZE - 1, (_pointx + _moveSpeed) / TILESIZE).type != 1)
 				{
 					_whereIsWorm = 1;
+					_image = IMAGEMANAGER->findImage("wormMoveLeft");
+					_pointx = _map->getMapInfo((_pointy) / TILESIZE - 1, (_pointx) / TILESIZE).rc.right + _image->getFrameWidth() / 2;
+					_pointy = _map->getMapInfo((_pointy) / TILESIZE - 1, (_pointx) / TILESIZE).rc.bottom;
+				}
+				else if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx + _moveSpeed) / TILESIZE).type == 1)
+				{
+					_whereIsWorm = 3;
 					_image = IMAGEMANAGER->findImage("wormMoveRight");
-					_pointx = _map->getMapInfo((_pointy) / TILESIZE, (_pointx + 1) / TILESIZE).rc.left - _image->getFrameWidth() / 2;
+					_pointx = _map->getMapInfo((_pointy) / TILESIZE, (_pointx + _moveSpeed) / TILESIZE).rc.left - _image->getFrameWidth() / 2;
+					_pointy = _map->getMapInfo((_pointy) / TILESIZE, (_pointx + _moveSpeed) / TILESIZE).rc.top;
 				}
 				else
 				{
@@ -182,18 +230,20 @@ void worm::move()
 			}
 			else
 			{
-				if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx - 1) / TILESIZE).type != 1 &&
-					_map->getMapInfo((_pointy) / TILESIZE - 1, (_pointx - 1) / TILESIZE).type != 1)
-				{
-					_whereIsWorm = 1;
-					_image = IMAGEMANAGER->findImage("wormMoveRight");
-					_pointx = _map->getMapInfo((_pointy) / TILESIZE+1, (_pointx) / TILESIZE).rc.left - _image->getFrameWidth() / 2;
-				}
-				else if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx - 1) / TILESIZE).type == 1)
+				if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx - _moveSpeed) / TILESIZE).type != 1 &&
+					_map->getMapInfo((_pointy) / TILESIZE - 1, (_pointx - _moveSpeed) / TILESIZE).type != 1)
 				{
 					_whereIsWorm = 3;
+					_image = IMAGEMANAGER->findImage("wormMoveRight");
+					_pointx = _map->getMapInfo((_pointy) / TILESIZE+1, (_pointx) / TILESIZE).rc.left - _image->getFrameWidth() / 2;
+					_pointy = _map->getMapInfo((_pointy) / TILESIZE + 1, (_pointx) / TILESIZE).rc.top;
+				}
+				else if (_map->getMapInfo((_pointy) / TILESIZE, (_pointx - _moveSpeed) / TILESIZE).type == 1)
+				{
+					_whereIsWorm = 1;
 					_image = IMAGEMANAGER->findImage("wormMoveLeft");
-					_pointx = _map->getMapInfo((_pointy) / TILESIZE, (_pointx - 1) / TILESIZE).rc.right + _image->getFrameWidth() / 2;
+					_pointx = _map->getMapInfo((_pointy) / TILESIZE, (_pointx - _moveSpeed) / TILESIZE).rc.right + _image->getFrameWidth() / 2;
+					_pointy = _map->getMapInfo((_pointy) / TILESIZE, (_pointx - _moveSpeed) / TILESIZE).rc.bottom;
 				}
 				else
 				{
@@ -204,18 +254,20 @@ void worm::move()
 		case 3:
 			if (_isLeft)
 			{
-				if (_map->getMapInfo((_pointy + 1) / TILESIZE, (_pointx) / TILESIZE).type != 1 &&
-					_map->getMapInfo((_pointy + 1) / TILESIZE, (_pointx) / TILESIZE + 1).type != 1)
-				{
-					_whereIsWorm = 2;
-					_image = IMAGEMANAGER->findImage("wormMoveDown");
-					_pointy = _map->getMapInfo((_pointy) / TILESIZE, (_pointx ) / TILESIZE + 1).rc.bottom + _image->getFrameHeight() / 2;
-				}
-				else if (_map->getMapInfo((_pointy + 1) / TILESIZE, (_pointx) / TILESIZE).type == 1)
+				if (_map->getMapInfo((_pointy + _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).type != 1 &&
+					_map->getMapInfo((_pointy + _moveSpeed) / TILESIZE, (_pointx) / TILESIZE + 1).type != 1)
 				{
 					_whereIsWorm = 0;
+					_image = IMAGEMANAGER->findImage("wormMoveDown");
+					_pointy = _map->getMapInfo((_pointy) / TILESIZE, (_pointx ) / TILESIZE + 1).rc.bottom + _image->getFrameHeight() / 2;
+					_pointx= _map->getMapInfo((_pointy) / TILESIZE, (_pointx) / TILESIZE + 1).rc.left;
+				}
+				else if (_map->getMapInfo((_pointy + _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).type == 1)
+				{
+					_whereIsWorm = 2;
 					_image = IMAGEMANAGER->findImage("wormMoveUp");
-					_pointy = _map->getMapInfo((_pointy + 1) / TILESIZE, (_pointx) / TILESIZE).rc.top - _image->getFrameHeight() / 2;
+					_pointy = _map->getMapInfo((_pointy + _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).rc.top - _image->getFrameHeight() / 2;
+					_pointx = _map->getMapInfo((_pointy + _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).rc.right;
 				}
 				else
 				{
@@ -224,18 +276,20 @@ void worm::move()
 			}
 			else
 			{
-				if (_map->getMapInfo((_pointy - 1) / TILESIZE, (_pointx) / TILESIZE).type != 1 &&
-					_map->getMapInfo((_pointy - 1) / TILESIZE, (_pointx) / TILESIZE + 1).type != 1)
-				{
-					_whereIsWorm = 0;
-					_image = IMAGEMANAGER->findImage("wormMoveUp");
-					_pointy = _map->getMapInfo((_pointy) / TILESIZE, (_pointx) / TILESIZE + 1).rc.top - _image->getFrameHeight() / 2;
-				}
-				else if (_map->getMapInfo((_pointy - 1) / TILESIZE, (_pointx) / TILESIZE).type == 1)
+				if (_map->getMapInfo((_pointy - _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).type != 1 &&
+					_map->getMapInfo((_pointy - _moveSpeed) / TILESIZE, (_pointx) / TILESIZE + 1).type != 1)
 				{
 					_whereIsWorm = 2;
+					_image = IMAGEMANAGER->findImage("wormMoveUp");
+					_pointy = _map->getMapInfo((_pointy) / TILESIZE, (_pointx) / TILESIZE + 1).rc.top - _image->getFrameHeight() / 2;
+					_pointx= _map->getMapInfo((_pointy) / TILESIZE, (_pointx) / TILESIZE + 1).rc.right;
+				}
+				else if (_map->getMapInfo((_pointy - _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).type == 1)
+				{
+					_whereIsWorm = 0;
 					_image = IMAGEMANAGER->findImage("wormMoveDown");
-					_pointy = _map->getMapInfo((_pointy - 1) / TILESIZE, (_pointx) / TILESIZE).rc.bottom + _image->getFrameHeight() / 2;
+					_pointy = _map->getMapInfo((_pointy - _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).rc.bottom + _image->getFrameHeight() / 2;
+					_pointx = _map->getMapInfo((_pointy - _moveSpeed) / TILESIZE, (_pointx) / TILESIZE).rc.left;
 				}
 				else
 				{
@@ -255,6 +309,7 @@ void worm::attack()
 }
 void worm::frameUpdate()
 {
+	_frameFPS = 10;
 	/*
 	switch (_state)
 	{
@@ -322,18 +377,18 @@ void worm::frameUpdate()
 		if (_isLeft)
 			_image->setFrameY(0);
 		else _image->setFrameY(1);
-		_frameTime += TIMEMANAGER->getElapsedTime();
-		if (_frameTime >= 1 / _frameFPS)
+		_frameTime ++;
+		if (_frameTime >= _frameFPS)
 		{
-			_frameTime -= 1 / _frameFPS;
+			_frameTime = 0;
 			if (_reverseFrame)
-				_currentFrameX++;
-			else
 				_currentFrameX--;
+			else
+				_currentFrameX++;
 			if (_currentFrameX > _image->getMaxFrameX())
 			{
 				_reverseFrame = true;
-				_currentFrameX = _image->getMaxFrameX()-1;
+				_currentFrameX = _image->getMaxFrameX();
 			}
 			else if (_currentFrameX < 0)
 			{
